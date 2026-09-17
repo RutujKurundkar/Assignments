4192b780ea8044024f18e1649c94ec21704d9959
RutujKurundkar <rutuj.kurundkar.cmaug26@gmail.com>
2026-09-18 00:23:21 +0530

    Add files via upload

---
+++ OOPJ/Q6.java
@@ -0,0 +1,10 @@
+package assignment5;
+
+public class Q6 {
+	public static void main (String[] args) {
+		String str = "The Quick BroWn FoX!";
+		System.out.println ("Original String: " + str);
+		String str_2 = str.toUpperCase();
+		System.out.println ("String in uppercase: " + str_2);
+}
+}
